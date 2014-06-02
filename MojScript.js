a078a532937b21954cc49a1d28750dc7bf8fdf0f
konrad2402 <konrad14@wp.pl>
2014-06-02 23:02:33 +0200

    wyłuskanie danych

--- MojScript.js
+++ MojScript.js
@@ -11,9 +11,41 @@ APP.ApplicationCtrl = function ($scope) {
         '"I":177,"B":1.4704,"S":1.4700,"P":0.45',
         '"I":305,"B":8.1674,"S":8.1611,"P":0.29'
     ];
-    var tablica = new Array();
-	$tablica =  $scope.cities;
-	console.log("" + $tablica);
+	var x = 0;
+    var tablica = new Array(10);
+	var tmp, tmp2; 
+	
+	for(x;x<$scope.cities.length;x++){
+		tablica[x] = new Array();
+	}
+	x=0;
+	for(x;x<$scope.cities.length;x++){
+		tablica[x] = $scope.cities[x];
+		tmp=tablica[x].indexOf(':',5);
+		tmp+=1;
+		tmp2=tablica[x].indexOf(',',13);
+		//$tablica[x].splice(0,9);
+		console.log(tablica[x].substring(tmp, tmp2) + "  " + tablica[0].substring(22,28) + "   " +  $scope.cities[x].length);
+	}
+	//var y = new Array();
+	//y = tablica[0];
+	//y.indeksOf ('"B":', 0);
+	//console.log(y.substring(tmp, tmp2));
+	//string  = "ABCDEFGHIJK	LŁMNOPRSTUWZY";
+//string.indexOf("K", 3);   // zwróci 10
+	//string  = "ABCDEFGHIJKLŁMNOPRSTUWZY";
+//window.alert(string.substring(5, 15));
+	//$tablica =  $scope.cities;
+	//console.log($scope.cities.length);
+/*	for(x;x<$tablica.length;x++){
+		console.log($tablica[x]);
+	}*/
+	/*for(x;x<$scope.cities.length;x++){
+		console.log($tablica[x].length);
+		var y=0;
+		$scope.cities[0].splice(0, 2);
+		
+	}*/
 };
 
 // http://mrzepinski.pl/angularjs-5-ng-repeat.html  <- filtry
