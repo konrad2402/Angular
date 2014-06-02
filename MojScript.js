1186197822253cec01dd6e8c533a973d41a07ee7
konrad2402 <konrad14@wp.pl>
2014-06-02 18:39:31 +0200

    Angular testy

---
+++ MojScript.js
@@ -0,0 +1,19 @@
+﻿var APP = angular.module('APP', []);
+
+APP.ApplicationCtrl = function ($scope) {
+    
+    $scope.name = '...';
+    
+    $scope.sayHelloWorld = function () {
+        $scope.name = 'World';
+    };
+    
+    $scope.sayHelloAngular = function () {
+        $scope.name = 'Angular';
+    };
+    
+    $scope.sayHelloSantaClaus = function () {
+        $scope.name = 'Santa Claus';
+    };
+    
+};
