8b05e6e034dd05c1d9cf3c9002446bce82be092e
konrad2402 <konrad14@wp.pl>
2014-06-03 14:24:47 +0200

    FILTR I SORT

--- MojScript.js
+++ MojScript.js
@@ -1,9 +1,24 @@
 ﻿var APP = angular.module('APP', []);
-
+APP.ApplicationCtrl = function ($scope) {
+	$.getJSON( "waluty.json", function(data) {
+		$scope.$apply(function(){
+            $scope.cities = data['Feeds'];
+			//console.log( data.length );
+		//console.log(data.Feeds[1])
+        }); 
+	})
+	$scope.Valuty = [
+		'EUR/PLN',
+		'USD/PLN',
+		'EUR/USD',
+		'EUR/CHF',
+		'EUR/JPY',
+		'EUR/GBP',
+		'EUR/AUD',
+		'EUR/NOK'
+	];
+};
 function Ctrl($scope) {
- //$scope.cities = new Array();
-	
-	var x = 0;	
 	$scope.Valuty = [
 		'EUR/PLN',
 		'USD/PLN',
