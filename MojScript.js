630c151f779d33316c62026863bd227d00003d22
konrad2402 <konrad14@wp.pl>
2014-06-03 16:08:02 +0200

    zrobione :D

--- MojScript.js
+++ MojScript.js
@@ -1,24 +1,4 @@
-﻿var APP = angular.module('APP', []);
-APP.ApplicationCtrl = function ($scope) {
-	$.getJSON( "waluty.json", function(data) {
-		$scope.$apply(function(){
-            $scope.cities = data['Feeds'];
-			//console.log( data.length );
-		//console.log(data.Feeds[1])
-        }); 
-	})
-	$scope.Valuty = [
-		'EUR/PLN',
-		'USD/PLN',
-		'EUR/USD',
-		'EUR/CHF',
-		'EUR/JPY',
-		'EUR/GBP',
-		'EUR/AUD',
-		'EUR/NOK'
-	];
-};
-function Ctrl($scope) {
+﻿function Ctrl($scope) {
 	$scope.Valuty = [
 		'EUR/PLN',
 		'USD/PLN',
@@ -30,13 +10,19 @@ function Ctrl($scope) {
 		'EUR/NOK'
 	];
 	
-	$.getJSON( "waluty.json", function(data) {
-		$scope.$apply(function(){
-            $scope.cities = data['Feeds'];
-			//console.log( data.length );
-		//console.log(data.Feeds[1])
-        }); 
-	})
+		$.getJSON( "waluty.json", function(data) {
+			$scope.$apply(function(){
+				$scope.cities = data['Feeds'];
+				//console.log( data.length );
+			//console.log(data.Feeds[1])
+			}); 
+		})
+	setTimeout('timedRefresh()', 5000);
+	function timedRefresh()
+	{
+		location.reload()
+		console.log("siemka");
+	}
 };
 
 // http://mrzepinski.pl/angularjs-5-ng-repeat.html    <- filtry
